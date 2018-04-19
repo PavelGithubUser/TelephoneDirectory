96a7fdc54fbbd2f917677b6fb09e4fd740fb7e48
Pavel <Pavel.WebOnline@gmail.com>
2018-04-20 00:31:03 +0300

    add class PhoneDirectory

---
+++ src/main/java/ru/otr/td/PhoneDirectory.java
@@ -0,0 +1,4 @@
+package ru.otr.td;
+
+public class PhoneDirectory {
+}
--- src/main/java/ru/otr/td/app/Application.java
+++ src/main/java/ru/otr/td/app/Application.java
@@ -1,6 +1,6 @@
-package ru.otr.td;
+package ru.otr.td.app;
 
-public class Main {
+public class Application {
     public static void main(String[] args) {
 
     }
